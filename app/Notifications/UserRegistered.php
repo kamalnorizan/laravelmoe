debd193b7831c8ab2ffa886fdba9f9627bdfe9e3
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-07-30 15:58:24 +0800

    update

--- app/Notifications/UserRegistered.php
+++ app/Notifications/UserRegistered.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserRegistered extends Notification
+class UserRegistered extends Notification implements ShouldQueue
 {
     use Queueable;
 
