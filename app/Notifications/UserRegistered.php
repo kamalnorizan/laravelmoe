291b0857462cef5e43a5cd45b675d7001817d50b
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-07-30 14:45:28 +0800

    notification

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserRegistered extends Notification
+{
+    use Queueable;
+
+    public string $password;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('Welcome to Our Application')
+                    ->greeting('Hello!')
+                    ->line('You have been successfully registered.')
+                    ->line('Your temporary password is: ' . $this->password)
+                    ->action('Please login', url('/login'))
+                    ->line('Thank you for using our application!')
+                    ->salutation('Best Regards, Your Application Team');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
